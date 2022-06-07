819e009a07150e02ad64c9ff0b39b56e74c22b6b
大嶋由真 <>
2022-06-07 16:24:44 +0900

    JavaScript課題1-1

---
+++ main.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 99; ++i) {
+  if (i % 15 === 0) {
+    console.log("FizzBuzz" + i);
+  } else if (i % 5 === 0) {
+    console.log("Buzz" + i);
+  } else if (i % 3 === 0) {
+    console.log("Fizz" + i);
+  }
+}
